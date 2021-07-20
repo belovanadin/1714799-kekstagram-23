6997da54d30da5acfaf3a912510bf604391bf926
belovanadin <req.of.hope@gmail.com>
2021-07-20 22:47:08 +0300

    фильтрация

--- js/utils.js
+++ js/utils.js
@@ -1,23 +1,25 @@
-
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
+const DELAY = 500;
 const checkStringLength = (string, length) => string.length <= length;
 
-const debounce = (callback, timeoutDelay = 500) => {
-  let timeoutId;
+const debounce = (cb) => {
+  let lastTimeout = null;
 
-  return (...rest) => {
-    clearTimeout(timeoutId);
-
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-
+  return (...args) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(...args);
+    }, DELAY);
   };
 };
 
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-export {getRandomPositiveInteger, checkStringLength, debounce};
+export {checkStringLength, debounce, shuffleArray};
